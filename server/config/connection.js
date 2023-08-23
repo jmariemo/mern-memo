b5bfe63f0821cdf77c152b79db994feb8ead9a73
Vivian Kuo <vivian.kuo4@outlook.com>
2023-08-23 11:54:18 -1000

    initial commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGOB_URI || 'mongodb://localhost/memo', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
