daa0a364ccca5e4fb4b1c72a6c969d206c34aff9
Vivian Kuo <vivian.kuo4@outlook.com>
2023-08-24 15:44:14 -1000

    added content

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,8 @@
+// import Profile model
+const { Profile } = require('../models');
+
+// import sign token function from auth
+const { signToken } = require('../utils/auth');
+    
+    // pulling a contact through the profile by either their last name or
+
