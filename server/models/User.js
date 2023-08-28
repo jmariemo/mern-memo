77aa74a2d99974c6fd481602fead39b123a3ff90
Vivian Kuo <vivian.kuo4@outlook.com>
2023-08-27 19:01:56 -1000

    delete "Profile", changed to "User"

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 const bcrypt = require("bcrypt");
@@ -52,3 +53,64 @@ userSchema.virtual("contactCount").get(function () {
 const User = model("User", userSchema)
 
 module.exports = User;
+=======
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// import schema from Contact.js
+const contactSchema = require('./Contact');
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+    },
+    zipCode: {
+      type: String,
+      required: true,
+    },
+    savedContacts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Contacts'
+      }
+    ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+profileSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+profileSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+profileSchema.virtual('contacts').get(function () {
+    return this.contactSchema.length;
+});
+
+const User = model('Profile', userSchema );
+
+module.exports = User; 
+>>>>>>> a641f21 (delete "Profile", changed to "User")
