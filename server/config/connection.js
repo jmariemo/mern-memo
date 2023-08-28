32775e0d2a1445a26dfb348e7be2e87d3d1832e7
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-08-27 17:52:59 -0700

    connection memo

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", false);
 // mongoose.connect(process.env.MONGO_URL);
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB',
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/memo',
     {
       useNewUrlParser: true, 
       useUnifiedTopology: true,
