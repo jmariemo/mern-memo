e2ac4656de411179ed3e114b283fa8885e12905f
Vivian Kuo <vivian.kuo4@outlook.com>
2023-08-27 19:09:44 -1000

    changed to "Event"

--- server/models/Contact.js
+++ server/models/Contact.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema, model } = require('mongoose');
-const eventSchema = require('./Events');
+const eventSchema = require('./Event');
 
 const contactSchema = new Schema({
   // Profile that the contact belongs to
